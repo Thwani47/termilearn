e06a6a8a3649d7ac5e48bb7455759cba7dccfe2a
Thwani sithole <thulaniinnocent47@gmail.com>
2024-07-07 20:32:14 +0200

    setup tabs but they are not working correctly

--- termilearn.go
+++ termilearn.go
@@ -2,78 +2,113 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
-	"time"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-	status int
-	err    error
-}
-
-const url = "https://charm.sh"
-
-func checkServer() tea.Msg {
-	c := &http.Client{Timeout: 10 * time.Second}
-	res, err := c.Get(url)
-
-	if err != nil {
-		return errMsg{err}
-	}
-
-	return statusMessage(res.StatusCode)
-}
-
-type statusMessage int
-
-type errMsg struct{ err error }
-
-func (e errMsg) Error() string {
-	return e.err.Error()
+	Tabs       []string
+	TabContent []string
+	activeTab  int
 }
 
 func (m model) Init() tea.Cmd {
-	return checkServer
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case statusMessage:
-		m.status = int(msg)
-		return m, tea.Quit
-	case errMsg:
-		m.err = msg.err
-		return m, tea.Quit
-
 	case tea.KeyMsg:
-		if msg.Type == tea.KeyCtrlC {
+		switch keypress := msg.String(); keypress {
+		case "ctrl+c", "q":
 			return m, tea.Quit
+		case "right", "l", "n", "tab":
+			m.activeTab = min(m.activeTab+1, len(m.Tabs)-1)
+			return m, nil
+		case "left", "h", "p", "shift+tab":
+			m.activeTab = max(m.activeTab-1, 0)
+			return m, nil
 		}
 	}
 
 	return m, nil
 }
 
+func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
+	border := lipgloss.RoundedBorder()
+	border.BottomLeft = left
+	border.Bottom = middle
+	border.BottomRight = right
+	return border
+}
+
+var (
+	inactiveTabBorder = tabBorderWithBottom("┴", "─", "┴")
+	activeTabBorder   = tabBorderWithBottom("┘", " ", "└")
+	docStyle          = lipgloss.NewStyle().Padding(1, 2, 1, 2)
+	highlightColor    = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
+	inactiveTabStyle  = lipgloss.NewStyle().Border(inactiveTabBorder, true).BorderForeground(highlightColor).Padding(0, 1)
+	activeTabStyle    = inactiveTabStyle.Border(activeTabBorder, true)
+	windowStyle       = lipgloss.NewStyle().BorderForeground(highlightColor).Padding(2, 0).Align(lipgloss.Center).Border(lipgloss.NormalBorder()).UnsetBorderTop()
+)
+
 func (m model) View() string {
-	if m.err != nil {
-		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
+	doc := strings.Builder{}
+
+	var renderedTabs []string
+
+	for i, t := range m.Tabs {
+		var style lipgloss.Style
+		isFirst, isLast, isActive := i == 0, i == len(m.Tabs)-1, i == m.activeTab
+		if isActive {
+			style = activeTabStyle
+		} else {
+			style = inactiveTabStyle
+		}
+		border, _, _, _, _ := style.GetBorder()
+		if isFirst && isActive {
+			border.BottomLeft = "│"
+		} else if isFirst && !isActive {
+			border.BottomLeft = "├"
+		} else if isLast && isActive {
+			border.BottomRight = "│"
+		} else if isLast && !isActive {
+			border.BottomRight = "┤"
+		}
+		style = style.Border(border)
+		renderedTabs = append(renderedTabs, style.Render(t))
 	}
 
-	s := fmt.Sprintf("checking %s....\n\n", url)
-
-	if m.status > 0 {
-		s += fmt.Sprintf("%d %s!\n\n", m.status, http.StatusText(m.status))
-	}
-
-	return "\n" + s + "\n"
+	row := lipgloss.JoinHorizontal(lipgloss.Top, renderedTabs...)
+	doc.WriteString(row)
+	doc.WriteString("\n")
+	doc.WriteString(windowStyle.Width((lipgloss.Width(row) - windowStyle.GetHorizontalFrameSize())).Render(m.TabContent[m.activeTab]))
+	return docStyle.Render(doc.String())
 }
 
 func main() {
-	if _, err := tea.NewProgram(model{}).Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	tabs := []string{"Lip Gloss", "Blush", "Eye Shadow", "Mascara", "Foundation"}
+	tabContent := []string{"Lip Gloss Tab", "Blush Tab", "Eye Shadow Tab", "Mascara Tab", "Foundation Tab"}
+	m := model{Tabs: tabs, TabContent: tabContent}
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
 		os.Exit(1)
 	}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
