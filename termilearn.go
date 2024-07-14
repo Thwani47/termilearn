b1477d0b25445a1580aa9715a16ad1e251b8eaa3
Thwani sithole <thulaniinnocent47@gmail.com>
2024-07-14 15:26:14 +0200

    added bsic list of concepts

--- termilearn.go
+++ termilearn.go
@@ -2,26 +2,77 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+var (
+	docStyle          = lipgloss.NewStyle().Margin(1, 2)
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+type item struct {
+	title, desc string
+}
+
+func (i item) Title() string {
+	return i.title
+}
+
+func (i item) Description() string {
+	return i.desc
+}
+
+func (i item) FilterValue() string {
+	return i.title
+}
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprintf(w, fn(str))
+}
+
 type model struct {
+	list      list.Model
 	title     string
+	choice    string
 	err       error
+	quitting  bool
 	altScreen bool
 }
 
 type editorFinishedMsg struct {
 	err error
-}
-
-func InitModel() model {
-	return model{
-		title: "Termilearn",
-	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -42,34 +93,60 @@ func openEditor() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "a":
-			m.altScreen = !m.altScreen
-			cmd := tea.EnterAltScreen
-			if !m.altScreen {
-				cmd = tea.ExitAltScreen
-			}
-
-			return m, cmd
-		case "e":
+		if msg.String() == "e" {
 			return m, openEditor()
-		case "ctrl+c", "q":
+		}
+
+		if msg.String() == "ctrl+c" || msg.String() == "q" {
+			m.quitting = true
 			return m, tea.Quit
 		}
+
+		if msg.String() == "enter" {
+			i, ok := m.list.SelectedItem().(item)
+			if ok {
+				m.choice = string(i.title)
+			}
+			return m, nil
+		}
+
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
 	}
-	return m, nil
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
-	if m.err != nil {
-		return "Error: " + m.err.Error() + "\n"
+	if m.choice != "" {
+		return quitTextStyle.Render(fmt.Sprintf("You chose: %s\n", m.choice))
 	}
 
-	return m.title
+	if m.quitting {
+		return quitTextStyle.Render("Goodbye!\n")
+	}
+	return docStyle.Render(m.list.View())
 }
 
 func main() {
-	if _, err := tea.NewProgram(InitModel()).Run(); err != nil {
+	items := []list.Item{
+		item{title: "Hello World", desc: "Learn how to print the classical 'Hello World' message in Go"},
+		item{title: "Values", desc: "Learn about the basic types in Go"},
+		item{title: "Variables", desc: "Learn how to declare and initialize variables in Go"},
+		item{title: "Constants", desc: "Learn how to declare and initialize constants in Go"},
+		item{title: "For", desc: "Learn how to loop in Go"},
+		item{title: "If/Else", desc: "Learn how to use conditional statements in Go"},
+		item{title: "Switch", desc: "Learn how to use switch statements in Go"},
+	}
+
+	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
+	m.list.Title = "Go Concepts"
+	m.list.Styles.Title = titleStyle
+	m.list.Styles.PaginationStyle = paginationStyle
+	if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v", err)
 		os.Exit(1)
 	}
